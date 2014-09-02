d138992a4d560f9def3b6941aaa403fb75724a49
mihaiv <HelloMihai@gmail.com>
2014-09-02 18:17:06 -0400

    added Z transformations

--- exampleSource/HMSideDrawerDirectionalExample/HMBasicAnimation.h
+++ exampleSource/HMSideDrawerDirectionalExample/HMBasicAnimation.h
@@ -11,10 +11,13 @@
 static NSString* const HMBasicAnimation_OPACITY       = @"opacity";
 static NSString* const HMBasicAnimation_ROTATION_Y    = @"transform.rotation.y";
 static NSString* const HMBasicAnimation_ROTATION_X    = @"transform.rotation.x";
+static NSString* const HMBasicAnimation_ROTATION_Z    = @"transform.rotation.z";
 static NSString* const HMBasicAnimation_SCALE_X       = @"transform.scale.x";
 static NSString* const HMBasicAnimation_SCALE_Y       = @"transform.scale.y";
+static NSString* const HMBasicAnimation_SCALE_Z       = @"transform.scale.z";
 static NSString* const HMBasicAnimation_TRANSLATION_X = @"transform.translation.x";
 static NSString* const HMBasicAnimation_TRANSLATION_Y = @"transform.translation.y";
+static NSString* const HMBasicAnimation_TRANSLATION_Z = @"transform.translation.z";
 
 @interface HMBasicAnimation : NSObject
 
